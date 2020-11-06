dc27301408fa549736f4a5ad767b4bcf9a90f0b3
zxhjames <1849483422@qq.com>
2020-11-06 08:52:32 +0800

    go客户端服务

--- client/main.go
+++ client/main.go
@@ -3,8 +3,8 @@ package main
 import (
 	"fmt"
 	"github.com/gin-gonic/gin"
-	"google.golang.org/grpc"
 	pb "go_grpc/pb"
+	"google.golang.org/grpc"
 	"log"
 	"net/http"
 )
@@ -18,26 +18,20 @@ func main() {
 	defer conn.Close()
 	client := pb.NewGreeterClient(conn)
 
-	// Set up a http server.
 	r := gin.Default()
 	r.GET("/", func(c *gin.Context) {
-		//name := c.Param("name")
-
-		// Contact the server and print out its response.
-		req := &pb.TestRequest{Num1: 10,Num2: 10}
-		res, err := client.TestSomeThing(c, req)
+		req := &pb.AddRequest{Num1: 10,Num2: 10}
+		res, err := client.RemoteAdd(c, req)
 		if err != nil {
 			c.JSON(http.StatusInternalServerError, gin.H{
 				"error": err.Error(),
 			})
 			return
 		}
-
 		c.JSON(http.StatusOK, gin.H{
-			"result": fmt.Sprint(res.Message),
+			"result": fmt.Sprint(res.Answer),
 		})
 	})
-
 	// Run http server
 	if err := r.Run(":8052"); err != nil {
 		log.Fatalf("could not run server: %v", err)
--- grpc/server.go
+++ grpc/server.go
@@ -1,10 +1,10 @@
 package main
 
 import (
+	pb "go_grpc/pb"
 	"golang.org/x/net/context"
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/reflection"
-	pb "go_grpc/pb"
 	"log"
 	"net"
 )
@@ -12,9 +12,17 @@ import (
 // server is used to implement helloworld.GreeterServer.
 type server struct{}
 
+func (s *server) RemoteAdd(ctx context.Context, request *pb.AddRequest) (*pb.AddResponse, error) {
+	panic("implement me")
+}
+
+func (s *server) mustEmbedUnimplementedGreeterServer() {
+	panic("implement me")
+}
+
 // SayHello implements helloworld.GreeterServer
-func (s *server) SayHello(ctx context.Context, in *pb.TestRequest) (*pb.TestRequest, error) {
-	return &pb.TestRequest{Num1:10,Num2: 10}, nil
+func (s *server) SayHello(ctx context.Context, in *pb.AddRequest) (*pb.AddRequest, error) {
+	return &pb.AddRequest{Num1:10,Num2: 10}, nil
 }
 
 func main() {
@@ -23,7 +31,7 @@ func main() {
 		log.Fatalf("failed to listen: %v", err)
 	}
 	s := grpc.NewServer()
-	pb.RegisterGreeterServer(s,&server{})
+	//pb.RegisterGreeterServer(s,&server{})
 
 	// Register reflection service on gRPC server.
 	reflection.Register(s)
